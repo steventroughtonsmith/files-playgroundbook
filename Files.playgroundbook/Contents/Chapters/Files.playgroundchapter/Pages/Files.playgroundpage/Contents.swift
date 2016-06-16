3671d589b74798dd496aada8a2fd203f48e4e23e
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2016-06-17 00:10:43 +0100

    First pass

---
+++ Files.playgroundbook/Contents/Chapters/Files.playgroundchapter/Pages/Files.playgroundpage/Contents.swift
@@ -0,0 +1,129 @@
+/*
+File Browser for Swift Playgrounds
+*/
+
+import UIKit
+import QuickLook
+import PlaygroundSupport
+
+class FBFilesTableViewController : UITableViewController, QLPreviewControllerDataSource
+{
+	var files = Array<String>()
+	var path = "/"
+	
+	required init(path: String)
+	{
+		super.init(style: .plain)
+		self.title = (path as NSString).lastPathComponent
+		
+		do
+		{
+			self.files = try FileManager.default().contentsOfDirectory(atPath: path)
+		}
+		catch _
+		{
+			if path == "/System"
+			{
+				self.files = ["Library"]
+			}
+		}
+		
+		let label = NSString(format: "%lu items", self.files.count)
+		let itemCountBarItem = UIBarButtonItem(title: label as String, style: .plain, target: nil, action: nil)
+		let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		
+		self.setToolbarItems([flexibleSpace, itemCountBarItem, flexibleSpace], animated: false)
+		
+		self.path = path
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+	
+	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 72
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return files.count
+	}
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = UITableViewCell()
+		var newPath : NSString = (self.path as NSString).appendingPathComponent(files[indexPath.row])
+		
+		cell.textLabel?.text = newPath.lastPathComponent
+		cell.imageView?.tintColor = UIColor(colorLiteralRed: 0.565, green: 0.773, blue: 0.961, alpha: 1.0)
+		
+		var isDirectory = ObjCBool(false)
+		FileManager.default().fileExists(atPath:newPath as String, isDirectory: &isDirectory)
+		
+		if isDirectory
+		{
+			cell.imageView?.image = UIImage(named: "Folder")
+		}
+		else if (newPath as NSString).pathExtension == "png"
+		{
+			cell.imageView?.image = UIImage(named: "Picture")
+		}
+		else
+		{
+			cell.imageView?.image = UIImage(named: "Document")
+		}
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		var newPath = self.path as NSString
+		newPath = newPath.appendingPathComponent(files[indexPath.row])
+		
+		var isDirectory = ObjCBool(false)
+		FileManager.default().fileExists(atPath:newPath as String, isDirectory: &isDirectory)
+		
+		if isDirectory
+		{
+			let tableVC = FBFilesTableViewController(path:newPath as String)
+			self.navigationController?.pushViewController(tableVC, animated: true)
+		}
+		else
+		{
+			let previewVC = QLPreviewController()
+			previewVC.dataSource = self
+			self.navigationController?.pushViewController(previewVC, animated: true)
+		}
+	}
+	
+	func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+		return 1
+	}
+	
+	func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+		var newPath = self.path as NSString
+		newPath = newPath.appendingPathComponent(files[self.tableView.indexPathForSelectedRow!.row])
+		
+		return URL(fileURLWithPath: newPath as String)
+	}
+}
+
+
+let rootView = UIView()
+let tableVC = FBFilesTableViewController(path:"/")
+
+let navVC = UINavigationController(rootViewController: tableVC)
+navVC.isToolbarHidden = false
+
+let window = UIWindow()
+window.rootViewController = navVC
+window.makeKeyAndVisible()
+
+window.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+
+PlaygroundPage.current.liveView = window
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# File Browser for Swift Playgrounds
+
+[![](https://lh3.googleusercontent.com/-ojqMcuzPVzc/V2MxkidUqXI/AAAAAAAACOU/cyMuQMBeP_YOAw0egiaWKE7ijvAxfaF6wCCo/s1800/IMG_0322.PNG)](https://lh3.googleusercontent.com/-ojqMcuzPVzc/V2MxkidUqXI/AAAAAAAACOU/cyMuQMBeP_YOAw0egiaWKE7ijvAxfaF6wCCo/s1800/IMG_0322.PNG)
